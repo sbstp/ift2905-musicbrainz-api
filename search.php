9e78b83e500037d08734e1212fb0a652c5b3c0c5
Simon Bernier St-Pierre <sbernierstpierre@gmail.com>
2016-04-04 23:20:23 -0400

    add artist search

---
+++ search.php
@@ -0,0 +1,19 @@
+<?php
+include('common.php');
+
+$query = $_GET['query'];
+
+$res = pg_query_params($conn, "select gid as id, name, comment from artist where name ilike $1 limit 10", array("%$query%"));
+$artists = pg_fetch_all($res);
+
+$query_len = strlen($query);
+
+// Sort by length of name cloest to query length.
+usort($artists, function($a, $b) use($query_len) {
+    $delta_a = abs(strlen($a['name']) - $query_len);
+    $delta_b = abs(strlen($b['name']) - $query_len);
+    return $delta_a - $delta_b;
+});
+
+header("Content-Type: application/json");
+echo json_encode($artists);
